f2908b2a60220f4d962f8154f59b4a47a30bd8b3
Mathieu Lavigne <mathieu.lavigne@capgemini.com>
2017-06-19 18:17:08 +0200

    Déplacement de méthodes dans utils.js

--- js/utils.js
+++ js/utils.js
@@ -41,3 +41,58 @@ function getCtrl() {
     if (typeof(angular) === undefined) document.location.reload(); // FIXME
     return angular.element(document.getElementById('ctrl')).scope();
 }
+
+/* global Notification */
+/**
+ * @author
+ */
+// TODO : doc pour Chrome : https://developer.chrome.com/apps/richNotifications
+function notify(message, options) {
+
+    /**
+     * dir : Le sens du texte de la notification ; Ce peut être auto, ltr, or rtl.
+     * lang : Spécifie la langue utilisée dans la notification. Cette chaîne doit être un BCP 47 language tag.
+     * body : Une chaîne représentant un contenu supplémentaire à afficher dans la notification.
+     * tag : Un identifiant pour une notification donnée qui permet de la récupérer, la remplacer ou la supprimer si besoin.
+     * icon : l'URL d'une image à utiliser comme icône par la notification
+     */
+    options = {
+        lang: options.lang || 'fr-FR',
+        tag: options.tag || 'm3u-player',
+        icon: options.icon || "https://img.youtube.com/vi/"+getCtrl().getVideoId()+"/default.jpg" //'/img/M3U.png' // http://stackoverflow.com/a/2068371/1655155
+    };
+
+    // Voyons si le navigateur supporte les notifications
+    if (!("Notification" in window)) {
+        alert("Ce navigateur ne supporte pas les notifications desktop");
+    }
+
+    // Voyons si l'utilisateur est OK pour recevoir des notifications
+    else if (Notification.permission === "granted") {
+        // Si c'est ok, créons une notification
+        var notification = new Notification(message, options);
+    }
+
+    // Sinon, nous avons besoin de la permission de l'utilisateur
+    // Note : Chrome n'implémente pas la propriété statique permission
+    // Donc, nous devons vérifier s'il n'y a pas 'denied' à la place de 'default'
+    else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function (permission) {
+
+            // Quelque soit la réponse de l'utilisateur, nous nous assurons de stocker cette information
+            if(!('permission' in Notification)) {
+                Notification.permission = permission;
+            }
+
+            // Si l'utilisateur est OK, on crée une notification
+            if (permission === "granted") {
+                var notification = new Notification(message, options);
+            }
+        });
+    }
+
+    // Comme ça, si l'utlisateur a refusé toute notification, et que vous respectez ce choix,
+    // il n'y a pas besoin de l'ennuyer à nouveau.
+
+    // TODO : notification.onclick = ...
+}
