f16e4049e4c5ce01571a28d9edec6b73e9bbb21a
Mathieu Lavigne <mathieu.lavigne@capgemini.com>
2017-07-28 13:26:04 +0200

    Début de TU du Controller #48

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,10 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      // Angular
+      'bower_components/angular/angular.min.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+
       'client/js/*.js',
       'client/js/controllers/*.js',
       'test/**/*.js',
