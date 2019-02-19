11f3103dcfc02beea64effb04e126816da7c0bb8
Bludwarf <bludwarf@gmail.com>
2019-02-19 19:05:00 +0100

    FIX 168 Une collection tout juste créée n'apparait pas dans la liste des collections
    Migration des anciens TU JS -> TS

---
+++ src/scripts/utils.spec.ts
@@ -0,0 +1,22 @@
+describe('utils', function () {
+
+  it('should replace url param', function () {
+    let src = 'http://localhost:3000/player?collection=from';
+    expect(setParameterByName('collection', 'to', src)).toEqual('http://localhost:3000/player?collection=to');
+    expect(src).toEqual('http://localhost:3000/player?collection=from');
+
+    src = 'http://localhost:3000/player?collection=from&collection2=from2';
+    expect(setParameterByName('collection', 'to', src)).toEqual('http://localhost:3000/player?collection=to&collection2=from2');
+
+    src = 'http://localhost:3000/player?collection1=from&collection2=from2';
+    expect(setParameterByName('collection', 'to', src))
+      .toEqual('http://localhost:3000/player?collection1=from&collection2=from2&collection=to');
+
+    src = 'http://localhost:3000/player';
+    expect(setParameterByName('collection', 'to', src)).toEqual('http://localhost:3000/player?collection=to');
+
+    const oldSearch = window.location.search;
+    expect(window.location.search).toEqual(oldSearch);
+  });
+
+});
