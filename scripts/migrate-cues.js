38d6ffc46b80394e03230496c2ad7468426c791c
LAVIGNE Mathieu <m.lavigne@rennesmetropole.fr>
2026-01-30 10:34:25 +0100

    feat: #193 TODO migrate-cues.js

--- scripts/migrate-cues.js
+++ scripts/migrate-cues.js
@@ -1,19 +1,39 @@
 const path = require("path");
 const fs = require('fs');
 const mkdirp = require('mkdirp');
-const CueService = require('../services/CueService');
+// const CueService = require('../services/CueService');
 
 const root = path.resolve(__dirname, "..");
-const dir = root + "/client/cues/";
+const dir = "C:\\Users\\m.lavigne\\Desktop\\Disques-20260115T080945Z-1-001\\Disques";
+const moveMode = true;
 
 // On prend tous les fichiers cue à la racine et on les met dans des dossiers comme cuetube-cues
 fs.readdir(dir, (err, filenames) => {
     filenames.forEach(filename => {
         const src = path.join(dir, filename);
-        const outDir = path.join(dir, encode(filename[0]), encode(filename[1]), encode(filename[2]));
-        const dest = CueService.getPath(filename);
-        mkdirp.sync(outDir);
-        fs.createReadStream(src).pipe(fs.createWriteStream(dest));
-        console.log(`${src} copié vers ${dest}`);
+        if (src.endsWith(".cue")) {
+            const outDir = path.join(dir, encode(filename[0]), encode(filename[1]), encode(filename[2]));
+            const dest = CueServiceGetPath(filename);
+            mkdirp.sync(outDir);
+
+            if (moveMode) {
+                fs.renameSync(src, dest)
+            } else {
+                fs.createReadStream(src).pipe(fs.createWriteStream(dest));
+            }
+
+            const actionName = moveMode ? 'déplacé' : 'copié';
+            console.log(`${src} ${actionName} vers ${dest}`);
+        }
     });
 });
+
+// TODO : pour éviter d'avoir à corriger les dépendances de la source : services/CueService.js
+function CueServiceGetPath(filename) {
+    const outDir = path.join(dir, encode(filename[0]), encode(filename[1]), encode(filename[2]));
+    return path.join(outDir, filename);
+}
+
+function encode(char) {
+    return char.toUpperCase();
+}
